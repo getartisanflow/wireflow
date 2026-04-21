4e45a75ac0c028ce9e1e35185b3a63918adf58b5
zachiler <79065463+zachiler@users.noreply.github.com>
2026-04-21 09:08:37 -0400

    fix(schema): nest inspector aside inside flow-container so it inherits canvas scope

--- resources/views/components/schema-designer.blade.php
+++ resources/views/components/schema-designer.blade.php
@@ -17,37 +17,38 @@
         :config="$presetConfig"
     >
         {{ $slot }}
+
+        {{-- Inspector aside lives INSIDE flow-container so it inherits the canvas x-data scope. --}}
+        <aside class="flow-schema-designer-inspector">
+            <div x-show="selectedNodes?.size > 0" x-transition>
+                @if($hasNodeInspector)
+                    {{ $nodeInspector }}
+                @else
+                    <div x-schema-node-inspector>
+                        <template x-schema-default-ui></template>
+                    </div>
+                @endif
+            </div>
+
+            <div x-show="selectedRows?.size > 0" x-transition>
+                @if($hasRowInspector)
+                    {{ $rowInspector }}
+                @else
+                    <div x-schema-row-inspector>
+                        <template x-schema-default-ui></template>
+                    </div>
+                @endif
+            </div>
+
+            <div x-show="selectedEdges?.size > 0" x-transition>
+                @if($hasEdgeInspector)
+                    {{ $edgeInspector }}
+                @else
+                    <div x-schema-edge-inspector>
+                        <template x-schema-default-ui></template>
+                    </div>
+                @endif
+            </div>
+        </aside>
     </x-flow>
-
-    <aside class="flow-schema-designer-inspector" x-data>
-        <div x-show="$data.selectedNodes?.size > 0" x-transition>
-            @if($hasNodeInspector)
-                {{ $nodeInspector }}
-            @else
-                <div x-schema-node-inspector>
-                    <template x-schema-default-ui></template>
-                </div>
-            @endif
-        </div>
-
-        <div x-show="$data.selectedRows?.size > 0" x-transition>
-            @if($hasRowInspector)
-                {{ $rowInspector }}
-            @else
-                <div x-schema-row-inspector>
-                    <template x-schema-default-ui></template>
-                </div>
-            @endif
-        </div>
-
-        <div x-show="$data.selectedEdges?.size > 0" x-transition>
-            @if($hasEdgeInspector)
-                {{ $edgeInspector }}
-            @else
-                <div x-schema-edge-inspector>
-                    <template x-schema-default-ui></template>
-                </div>
-            @endif
-        </div>
-    </aside>
 </div>
